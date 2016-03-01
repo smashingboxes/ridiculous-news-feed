854d22c50dc638c19f84bba6ac10d93cd21bb9d5
Keegan Brown <brown.keegan@gmail.com>
2016-02-29 18:21:32 -0600

    inital

--- index.js
+++ index.js
@@ -7,6 +7,79 @@ const config = require('./config');
 let search = 'funny cat';
 const app = express();
 
+function filterStopWords (arr) {
+  return arr;
+}
+
+function getTitleWordArray (title) {
+  return title.split(' ').filter((word) => {
+    return word.length > 4;
+  });
+}
+
+function getWord (title) {
+  let wordArray = getTitleWordArray(title);
+  return randItem(wordArray);
+}
+
+function randItem (arr) {
+  return arr[ Math.floor(Math.random()*arr.length) ];
+}
+
+function buildImageLoop (newsItemTitles, giphyURLS) {
+  let imageStack = [];
+  newsItemTitles.forEach((title, i) => {
+    imageStack.push(`
+      <div class="article">
+        <h2>${title}</h2>
+        <img src="${giphyURLS[i]}" alt="" />
+      </div>
+    `);
+  });
+  return imageStack.join(' \n');
+}
+
+function sendPageResponse(res, newsItemTitles, giphyURLS) {
+  res.end(`
+    <!DOCTYPE html><html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <title>Offensive News Feed</title>
+        <style>
+          html, body {
+            margin: 0;
+            font-family: sans-serif;
+          }
+          .article {
+            width: 100%;
+            height: 100vh;
+            position: relative;
+          }
+          .article h2 {
+            text-align: center;
+            padding: 20px;
+            background-color: #FFF;
+            position: absolute;
+            top: 60%;
+            right: 0;
+            font-size: 200%;
+          }
+          .article img {
+            display: block;
+            text-align: center;
+            height: 100vh;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="wrapper">
+          ${buildImageLoop(newsItemTitles, giphyURLS)}
+        </div>
+      </body>
+    </html>
+  `);
+}
+
 function setup() {
 
   app.get('/', (req, res) => {
@@ -22,18 +95,24 @@ function setup() {
 
         let asyncCounter = 0;
         let giphyURLS = [];
-        newsItemTitles.forEach((title) => {
 
+        newsItemTitles.forEach((title) => {
+          let search = getWord(title);
           request.get(`http://api.giphy.com/v1/gifs/search?limit=4&q=${encodeURIComponent(search)}&api_key=${config.API_KEY}`,
             (err, giphyRes) => {
+              asyncCounter++;
               let giphyData = JSON.parse(giphyRes.body).data;
               if ( giphyData.length ) {
-
+                console.log(giphyData);
+                giphyURLS.push(`https://media.giphy.com/media/${randItem(giphyData).id}/giphy.gif`);
+              } else {
+                giphyURLS.push('https://api.giphy.com/img/giphy_search.gif');
               }
-              res.json(giphyRes.body);
+              if (asyncCounter >= newsItemTitles.length) {
+                sendPageResponse(res, newsItemTitles, giphyURLS);
+              }
             });
         });
-
 
       });
 
