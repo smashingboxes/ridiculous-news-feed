87e09a35cc4f9f04e37cf748b1160832d78ae1b5
Keegan Brown <brown.keegan@gmail.com>
2016-06-16 14:34:38 -0500

    Adds GA and more

--- index.js
+++ index.js
@@ -51,6 +51,14 @@ function getPageContent (newsItems, hideSplash, footerAddon, titleAddon) {
           ${footerAddon||''}
         </div>
         <img class="powered-by-giphy" src="/powered-by-giphy.png" alt="" />
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+          ga('create', 'UA-29292622-22', 'auto');
+          ga('send', 'pageview');
+        </script>
       </body>
     </html>
   `;
